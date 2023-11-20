b8f82fb78db05e4b3352f36d1715adf3d995678b
gunnar.temnell <gunnar.temnell@temnellsolutions.se>
2023-11-20 13:36:47 +0100

    dag 8 svar 1

--- 2022-08/Program.cs
+++ 2022-08/Program.cs
@@ -17,61 +17,76 @@ string[,] _forrestDebug = new string[99, 99];
 Array.Clear(_forrestResult, 0, _forrestResult.Length);
 Array.Clear(_forrestDebug, 0, _forrestDebug.Length);
 
-for (int y = 0; y < _filedata.Length; y++)
+for (int _row = 0; _row < _filedata.Length; _row++)
 {
-    var _a = _filedata[y].Select(ch => ch - '0').ToArray();
-    for (int x = 0; x < _a.Length; x++)
+    var _rowData = _filedata[_row].Select(ch => ch - '0').ToArray();
+    for (int _col = 0; _col < _rowData.Length; _col++)
     {
-        _forrest[x, y] = Convert.ToByte(_a[x]);
+        _forrest[_row, _col] = Convert.ToByte(_rowData[_col]);
     }
 }
 
 int amount = 0;
 
-for (int cy = 0; cy < 99; cy++)
+for (int _cRow = 0; _cRow < 99; _cRow++)
 {
-    for (int cx = 0; cx < 99; cx++)
+    for (int _cCol = 0; _cCol < 99; _cCol++)
     {
-        amount += CheckIfVisible(_forrest, cx, cy);
+        amount += CheckIfVisible(_forrest, _cRow, _cCol);
     }
 }
 
+StringBuilder _sb = new StringBuilder();
+for (int _row = 0; _row < 99; _row++)
+{
+    for (int _col = 0; _col < 99; _col++)
+    {
+        _sb.Append(_forrestDebug[_row, _col]);
+    }
+    _sb.AppendLine();    
+}
 
-
+File.WriteAllText(@".\dataDebug.txt",_sb.ToString());
 Console.WriteLine($"S1 : {amount}");
 
-File.WriteAllText(@".\dataDebug.txt", _forrestDebug.ToString());
+File.WriteAllText(@"..\..\..\dataDebug.txt", _sb.ToString());
 
 const int SIZE = 99;
-int CheckIfVisible(byte[,] forrest, int treeX, int treeY)
+int CheckIfVisible(byte[,] forrest, int row, int col)
 {
-    if (treeX == 0 || treeX == SIZE - 1 || treeY == 0 || treeY == SIZE - 1)
+    if (row == 0 || row == SIZE - 1 || col == 0 || col == SIZE - 1)
     {
-        setDebugValue(_forrestDebug, treeX, treeY, forrest[treeX, treeY]);
+        setDebugValue(_forrestDebug, row, col, 1);
         return 1; // alla i ytterkant är synliga
     }
 
     // check from left
     int _cl = 1;
-    for (int i = 0; i < treeX; i++) if (forrest[i, treeY] >= forrest[treeX, treeY]) { _cl = 0; break; }
+    for (int i = 0; i < col; i++) if (forrest[row, i] >= forrest[row, col]) { _cl = 0; break; }
     // check from right
     int _cr = 1;
-    for (int i = SIZE - 1; i > treeX; i--) if (forrest[i, treeY] >= forrest[treeX, treeY]) { _cr = 0; break; }
+    for (int i = SIZE - 1; i > col; i--) if (forrest[row, i] >= forrest[row, col]) { _cr = 0; break; }
     // check from top
     int _ct = 1;
-    for (int i = 0; i < treeY; i++) if (forrest[i, treeX] >= forrest[treeX, treeY]) { _ct = 0; break; }
+    for (int i = 0; i < row; i++) if (forrest[i, col] >= forrest[row, col]) { _ct = 0; break; }
     // check from bottom
     int _cb = 1;
-    for (int i = SIZE - 1; i > treeY; i--) if (forrest[i, treeX] >= forrest[treeX, treeY]) { _cb = 0; break; }
+    for (int i = SIZE - 1; i > row; i--) if (forrest[i, col] >= forrest[row, col]) { _cb = 0; break; }
     int _r = (new[] { _cl, _cr, _ct, _cb }).Max();
 
-    setDebugValue(_forrestDebug, treeX, treeY, forrest[treeX, treeY]);
+    setDebugValue(_forrestDebug, row, col, _r);
     return _r;
 }
 
-void setDebugValue(string[,] _forrestDebug, int treeX, int treeY, byte value)
+void setDebugValue(string[,] _forrestDebug, int row, int col, int value)
 {
-    if (value >= 0) _forrestDebug[treeX, treeY] = value.ToString();
-    else _forrestDebug[treeX, treeY] = ".";
+    if (value > 0)
+    {
+        _forrestDebug[row, col] = _forrest[row, col].ToString();
+    }
+    else
+    {
+        _forrestDebug[row, col] = ".";
+    }
 
 }
