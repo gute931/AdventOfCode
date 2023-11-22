c7b2d3cd74f94839091c24d61c5b0eac3de16b41
gunnar.temnell <gunnar.temnell@temnellsolutions.se>
2023-11-22 21:12:53 +0100

    renderMap

--- 2022-09/Program.cs
+++ 2022-09/Program.cs
@@ -1,4 +1,5 @@
 ﻿using _2022_09;
+using System.Diagnostics;
 using System.Net.Security;
 using System.Numerics;
 using System.Reflection.Metadata.Ecma335;
@@ -26,15 +27,15 @@ ValueTuple<int?, int?> _prevHead = (0, 0);
 ValueTuple<int?, int?> _curTail = (null, null);
 
 // S1 : Declare
-SortedList<int, (int?, int?)> _rope = new SortedList<int, (int?, int?)>();
-
-
+SortedList<int, (int, int)> _rope = new SortedList<int, (int, int)>();
 _rope[0] = (0, 0);
+ValueTuple<int, int> _ropeHeadPrev = (0, 0);
+/*
 for (int i = 1; i < 10; i++)
 {
     _rope[i] = (null, null);
 }
-
+*/
 /*
 for (int i = 0; i < 10; i++)
 {
@@ -53,6 +54,7 @@ foreach (string _cmd in _filedata)
     for (int s = 0; s < _steps; s++)
     {
         _prevHead = _curHead;
+        _ropeHeadPrev = _rope[0];
 
         switch (_direction)
         {
@@ -75,7 +77,8 @@ foreach (string _cmd in _filedata)
         }
         saveHeadPositionS1();
         moveTailS1(_direction);
-        moveTailS2(_direction);
+        moveTailS2(_direction, _rowNo);
+        renderMap(_cmd, s + 1, _steps);
     }
 }
 
@@ -84,6 +87,75 @@ Console.WriteLine(_tailS1.Distinct().Count());
 Console.WriteLine(_tailS2.Distinct().Count());
 // 3668 Too high
 Console.ReadLine();
+
+
+
+void moveTailS2(string direction, int iterations)
+{
+    ValueTuple<int, int> _prev = _ropeHeadPrev;
+    ValueTuple<int, int> _prevTemp = (0, 0);
+    
+    if (_rope.Count < 10)
+    {
+        _rope[_rope.Count] = (0, 0); // Sätt in en bit till av repet.
+    }
+
+    for (int i = 1; i < _rope.Count; i++)
+    {   
+        _prevTemp = _rope[i-1];
+        int _diffRow = Math.Abs((int)(_rope[i - 1].Item1 - _rope[i].Item1));
+        int _diffCol = Math.Abs((int)(_rope[i - 1].Item2 - _rope[i].Item2));
+
+        if (_diffRow > 1 || _diffCol > 1)
+        {
+       
+            _rope[i] = _prev;
+            /* _rope[i] = (_rope[i - 1].Item1, _rope[i - 1].Item2); */
+            if (i == 9) saveTailPositionS2((int)_rope[i].Item1, (int)_rope[i].Item2);
+        }
+        _prev = _rope[i];
+    }
+}
+
+void renderMap(string cmd, int step, int _steps)
+{
+    Console.Clear();
+    for (int r = -15; r < 15; r++)
+    {
+        for (int c = -15; c < 15; c++)
+        {
+            string _sign = ".";
+            if (r == 0 && c == 0) _sign = "s";
+            for (int i = 0; i < _rope.Count; i++)
+            {
+                if (_rope[i].Item1 == r && _rope[i].Item2 == c)
+                {
+                    _sign = (i == 0) ? "H" : i.ToString();
+                    break;
+                }
+            }
+            Console.Write($"{_sign} ");
+        }
+        Console.WriteLine();
+
+    }
+    Console.WriteLine("------------------------------------");
+    Console.WriteLine($"Op:{cmd}, step:{step}/{_steps}");
+    Console.WriteLine("------------------------------------");
+}
+
+void saveHeadPositionS1()
+{
+    _headS1.Add($"{_curHead.Item1}:{_curHead.Item2}");
+}
+
+
+void saveTailPositionS2(int row, int col)
+{
+    _tailS2.Add($"{row}:{col}");
+}
+
+
 
 
 void moveTailS1(string direction)
@@ -106,41 +178,7 @@ void moveTailS1(string direction)
     }
 }
 
-void moveTailS2(string direction)
-{
-    for (int i = 1; i < 10; i++)
-    {
-        if (_curTail.Item1 == null)
-        {
-            _curTail = (0, 0); // Set origin initially 
-            saveTailPositionS2(0,0);
-        }
-        else
-        {
-            int _diffRow = Math.Abs((int)(_rope[i-1].Item1 - _rope[i].Item1));
-            int _diffCol = Math.Abs((int)(_rope[i - 1].Item2 - _rope[i].Item2));
-
-            if (_diffRow > 1 || _diffCol > 1)
-            {
-                _rope[i] = (_rope[i - 1].Item1, _rope[i - 1].Item2);
-                if (i == 9) saveTailPositionS2((int)_rope[i].Item1, (int)_rope[i].Item2);
-            }
-        }
-    }
-}
-
-
-void saveHeadPositionS1()
-{
-    _headS1.Add($"{_curHead.Item1}:{_curHead.Item2}");
-}
-
 void saveTailPositionS1()
 {
     _tailS1.Add($"{_curTail.Item1}:{_curTail.Item2}");
-}
-
-void saveTailPositionS2(int row, int col)
-{
-    _tailS2.Add($"{row}:{col}");
 }
