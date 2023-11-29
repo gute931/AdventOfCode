77694664a288456c08abeb3f5bb084624083f27c
gunnar.temnell <gunnar.temnell@temnellsolutions.se>
2023-11-29 20:20:56 +0100

    Strange error

--- 2022-12/Program.cs
+++ 2022-12/Program.cs
@@ -20,17 +20,21 @@ _nodes.Add(($"{GtConfig.Instance.STARTROW}:{GtConfig.Instance.STARTCOL}", new Gt
 */
 
 var graph = new Graph<int, string>();
-Dictionary<int, List<int>> _neighBoudList = new Dictionary<int, List<int>>();
+Dictionary<uint, List<uint>> _neighBoudList = new Dictionary<uint, List<uint>>();
 
 int _nodeR = 0;
-for (int _r = 0; _r < GtConfig.Instance.ROWS; _r++)
+for (uint _r = 0; _r < GtConfig.Instance.ROWS; _r++)
 {
     int _nodeC = 0;
-    for (int _c = 0; _c < GtConfig.Instance.COLS; _c++)
+    for (uint _c = 0; _c < GtConfig.Instance.COLS; _c++)
     {
-        int _node = (_r * 1000) + _c;
+        if (_r==20 && _c == 0 )
+        {
+            Console.WriteLine("Here!");
+        }
+        uint _node = (_r * 1000) + _c;
         char _currentLetter = GtConfig.Instance.Matrix[_r, _c];
-        List<int> neighb = new List<int>();
+        List<uint> neighb = new List<uint>();
         GtConfig.Instance.AddNeighrbourIfValid(neighb, _currentLetter, _r + 1, _c + 0);
         GtConfig.Instance.AddNeighrbourIfValid(neighb, _currentLetter, _r - 1, _c - 0);
         GtConfig.Instance.AddNeighrbourIfValid(neighb, _currentLetter, _r + 0, _c + 1);
@@ -38,7 +42,7 @@ for (int _r = 0; _r < GtConfig.Instance.ROWS; _r++)
         if (neighb.Count > 0) _neighBoudList.Add(_node, neighb);
     }
 }
-
+/*
 for (int _r = 0; _r < GtConfig.Instance.ROWS; _r++)
 {
     int _nodeC = 0;
@@ -49,24 +53,34 @@ for (int _r = 0; _r < GtConfig.Instance.ROWS; _r++)
     }
     _nodeR++;
 }
-
+*/
 
 foreach (var item in _neighBoudList)
 {
-    graph.AddNode(item.Key);
+    if (item.Key == 20000)
+    {
+        Console.WriteLine();
+    }
+    graph.AddNode((int)item.Key);
 }
 
 foreach (var item in _neighBoudList)
 {
+    if (item.Key == 20000)
+    {
+        Console.WriteLine();
+    }
     foreach (var _v in item.Value)
     {
+ 
         graph.Connect((uint)item.Key, (uint)_v, 1, $"{item.Key}:{_v}");
     }
 }
+ShortestPathResult result2 = graph.Dijkstra(1, 10);
 
 ShortestPathResult result = graph.Dijkstra(
-    (uint)((GtConfig.Instance.STARTROW * 1000) + GtConfig.Instance.STARTCOL),
-    (uint)((GtConfig.Instance.ENDROW * 1000) + GtConfig.Instance.ENDCOL)); //result contains the shortest path
+    (GtConfig.Instance.STARTROW * 1000) + GtConfig.Instance.STARTCOL,
+    (GtConfig.Instance.ENDROW * 1000) + GtConfig.Instance.ENDCOL, 200); //result contains the shortest path
 
 for (int _r = 0; _r < GtConfig.Instance.ROWS; _r++)
 {
