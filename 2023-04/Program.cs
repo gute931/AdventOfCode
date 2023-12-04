389dacd1a4b157039b4137b970bb87c6e692e93e
gunnar.temnell <gunnar.temnell@temnellsolutions.se>
2023-12-04 09:55:22 +0100

    2023-04

--- 2023-04/Program.cs
+++ 2023-04/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System.Text;
 
 Console.WriteLine("Uppgift 2023-12-04!");
-string[] _filedata = File.ReadAllLines("./testdata.txt");
+string[] _filedata = File.ReadAllLines("./data.txt");
 int[] _s2counter = new int[_filedata.Length];
 Array.Fill(_s2counter, 0);
 int SUM1 = 0;
@@ -9,10 +9,11 @@ int SUM2 = 0;
 foreach (string _rec in _filedata)
 {
     string[] _parts = _rec.Split(":|".ToCharArray(), StringSplitOptions.TrimEntries);
-    int _card = int.Parse(_parts[0].Split(' ', StringSplitOptions.RemoveEmptyEntries)[1]);
+    int _card = int.Parse(_parts[0].Split(' ', StringSplitOptions.RemoveEmptyEntries)[1]) - 1; // Zero 
     string[] _myNumbers = _parts[1].Split(' ', StringSplitOptions.RemoveEmptyEntries);
     string[] _winningNumbers = _parts[2].Split(' ', StringSplitOptions.RemoveEmptyEntries);
     var _m = _myNumbers.Intersect(_winningNumbers).Count();
+
     // Step 1
     int _p = 0;
     for (int i = 0; i < _m; i++)
@@ -21,13 +22,23 @@ foreach (string _rec in _filedata)
         else _p *= 2;
     }
     SUM1 += _p;
+
     // Step 2
-    _s2counter[_card - 1] += 1; // for current card
-    for (int i = 0; i < _m; i++)
-    {
-        _s2counter[_card + i] += 1; // for current card
-    }
-    SUM2 = _s2counter.Sum();
+    int[] _ra = new int[_m];
+    Array.Fill(_ra, 1); // Wins
+
+    // bonus
+    for (int i = 0; i < _m; i++) _ra[i] += _s2counter[_card];
+
+    // add points
+    for (int i = 1; i <= _m; i++)    _s2counter[_card + i] += _ra[i - 1];
+    
+    _s2counter[_card] += 1; // for current card
+}
+
+SUM2 = _s2counter.Sum();
+
 Console.WriteLine($"S1:{SUM1}");
 Console.WriteLine($"S2:{SUM2}");
-Console.WriteLine();
+
+Console.ReadLine();
