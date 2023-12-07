38f91fd8466a3f06e04a996b95f3f135d44811d6
gunnar.temnell <gunnar.temnell@temnellsolutions.se>
2023-12-07 08:39:06 +0100

    2023-07

--- 2022-12/Program.cs
+++ 2022-12/Program.cs
@@ -16,23 +16,8 @@ List<GtNode2> _nd = new List<GtNode2>();
 
 for (int _r = 0; _r < GtConfig.Instance.ROWS; _r++)
 {
-    for (int _c = 0; _c < GtConfig.Instance.COLS; _c++)
-    {
-        // _nd.Add(new GtNode2(_r, _c, _r, _c, GtConfig.Instance.Matrix[_r, _c]));
-        char[] _search = new char[2];
-        _search[0] = GtConfig.Instance.Matrix[_r, _c];
-        if (_search[0] == 'E')
-        {
-            _nd.Add(new GtNode2(_r, _c, _r, _c, _search[0]));
-        }
-       else if (_search[0] == 'S') _search[1] = 'a';
-        else if (_search[0] == 'z') _search[1] = 'E';
-        else _search[1] = (char)(((int)_search[0]) + 1);
-
-        GtNode2 nUp = GtConfig.Instance.ValidNeighbour(_r, _c, _r - 1, _c, _search);
-        GtNode2 nDown = GtConfig.Instance.ValidNeighbour(_r, _c, _r + 1, _c, _search);
-        GtNode2 nLeft = GtConfig.Instance.ValidNeighbour(_r, _c, _r, _c - 1, _search);
-        GtNode2 nRight = GtConfig.Instance.ValidNeighbour(_r, _c, _r, _c + 1, _search);
+    GtNode2 _root = new GtNode2(GtConfig.Instance.STARTROW, GtConfig.Instance.STARTCOL, Level, 0, localPath);
+_root.Search();
 
         if (nUp != null) _nd.Add(nUp);
         if (nDown != null) _nd.Add(nDown);
