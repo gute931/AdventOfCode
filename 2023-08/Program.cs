fd0fcc7b33065447d37bdcf0d5d1f93d10a06fbd
gunnar.temnell <gunnar.temnell@temnellsolutions.se>
2023-12-08 17:53:44 +0100

    2023-08 Klar

--- 2023-08/Program.cs
+++ 2023-08/Program.cs
@@ -1,34 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System.Data;
-using System.Net.Http.Headers;
-using System.Text;
+﻿using System.Text;
 
 Console.WriteLine("2023-12-08");
 string[] _data = File.ReadAllLines("./data.txt");
 
 char[] instructions = _data[0].Trim().ToCharArray();
-string _current = "";
 SortedList<string, (string, string)> map = new SortedList<string, (string, string)>();
 List<string> startStringsS1 = new List<string>();
 List<string> startStringsS2 = new List<string>();
-int _endsZ = 0;
 for (int i = 2; i < _data.Length; i++)
 {
     string[] _p = _data[i].Split(" =(,)".ToCharArray(), StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
-    if (i == 2) _current = _p[0];
     map.Add(_p[0], (_p[1], _p[2]));
     if (_p[0] == "AAA") startStringsS1.Add(_p[0]);
-    // if (_p[0].Last() == 'A' || _p[0].Last() == 'Z') Console.WriteLine(_p[0]);
     if (_p[0].Last() == 'A') startStringsS2.Add(_p[0]);
 }
 
 StringBuilder sb = new StringBuilder();
 
-;
-
 int SUM1 = 0;
-int SUM2 = 0;
 SUM1 = GetZ(startStringsS1[0], instructions, map, true);
+
 
 List<int> targets = new List<int>();
 for (int i = 0; i < startStringsS2.Count(); i++)
@@ -36,67 +27,13 @@ for (int i = 0; i < startStringsS2.Count(); i++)
     targets.Add(GetZ(startStringsS2[i], instructions, map, false));
 }
 
-for (int i = 3; i < 10000; i++)
-{
-    bool equal = true;
-    foreach (int item in targets)
-    {
-        if (item % i != 0)
-        {
-            equal = false;
-            break;
-        }
-    }
-    if (equal)
-    {
 
-        Console.WriteLine($"%={i}");
-        long _v = targets[0] * i;
-        if (targets.Where(w => w * i == _v).Count() == targets.Count)
-        {
-            Console.WriteLine($"SUM2_{_v}");
-        }
-        break;
-    }
-}
+long SUM2 = lcm_of_array_elements(targets.ToArray());
 
-List<long> _values = new List<long>();
-int _max = targets.Max();
-for (int i = 3000; i < 9999999; i++)
-{
-    _values.Add(_max * i);
-}
 
-foreach (var item in targets)
-{
-    Console.WriteLine($"--- Item : {item} ---");
-
-    for (int i = 3000; i < 9999; i++)
-    {
-        long _v = item * i;
-        if (_values.Contains(_v))
-        {
-            Console.WriteLine($"{i}:{_v}");
-        }
-        _values.Add(_max * i);
-    }
-}
-
-StringBuilder _sb = new StringBuilder();
-for (int i = 3000; i < 100000; i++)
-{
-    foreach (var item in targets)
-    {
-
-        _sb.AppendLine(string.Format("{0}\t{1}\t{2}", i, item, item * i));
-    }
-}
-File.WriteAllText(@"..\..\..\trace.txt", _sb.ToString());
 Console.WriteLine($"SUM1:{SUM1}");
-// 102758
-// 127506
 Console.WriteLine($"SUM2:{SUM2}");
-Console.WriteLine($"");
+Console.ReadLine();
 
 static int GetZ(string start, char[] instructions, SortedList<string, (string, string)> map, bool EndWithZZZ)
 {
@@ -132,6 +69,67 @@ static int GetZ(string start, char[] instructions, SortedList<string, (string, s
         }
     }
     return SUM;
+}
 
 
+long lcm_of_array_elements(int[] element_array)
+{
+    long lcm_of_array_elements = 1;
+    int divisor = 2;
+
+    while (true)
+    {
+
+        int counter = 0;
+        bool divisible = false;
+        for (int i = 0; i < element_array.Length; i++)
+        {
+
+            // lcm_of_array_elements (n1, n2, ... 0) = 0.
+            // For negative number we convert into
+            // positive and calculate lcm_of_array_elements.
+            if (element_array[i] == 0)
+            {
+                return 0;
+            }
+            else if (element_array[i] < 0)
+            {
+                element_array[i] = element_array[i] * (-1);
+            }
+            if (element_array[i] == 1)
+            {
+                counter++;
+            }
+
+            // Divide element_array by devisor if complete
+            // division i.e. without remainder then replace
+            // number with quotient; used for find next factor
+            if (element_array[i] % divisor == 0)
+            {
+                divisible = true;
+                element_array[i] = element_array[i] / divisor;
+            }
+        }
+
+        // If divisor able to completely divide any number
+        // from array multiply with lcm_of_array_elements
+        // and store into lcm_of_array_elements and continue
+        // to same divisor for next factor finding.
+        // else increment divisor
+        if (divisible)
+        {
+            lcm_of_array_elements = lcm_of_array_elements * divisor;
+        }
+        else
+        {
+            divisor++;
+        }
+
+        // Check if all element_array is 1 indicate 
+        // we found all factors and terminate while loop.
+        if (counter == element_array.Length)
+        {
+            return lcm_of_array_elements;
+        }
+    }
 }
