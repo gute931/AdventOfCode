3dc7bd13626a5fd99d33f29c6f6813cec950f42f
gunnar.temnell <gunnar.temnell@temnellsolutions.se>
2023-12-11 08:06:37 +0100

    bkp

---
+++ 2023-10/Coordinate.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _2023_10
+{
+    public class Coordinate
+    {
+        public char Symbol { get; set; }
+        public int Row { get; set; }
+        public int Col { get; set; }
+        public int RowTo { get; set; }
+        public int ColTo { get; set; }
+        public bool StartintPoint { get; set; } = false;
+        public Coordinate(char symbol, int row, int col, (int, int) to)
+        {
+            Symbol = symbol;
+            Row = row;
+            Col = col;
+            RowTo = to.Item1;
+            ColTo = to.Item2;
+        }
+        public Coordinate(char symbol, int row, int col)
+        {
+            StartintPoint = true;
+            Symbol = symbol;
+            Row = row;
+            Col = col;
+
+        }
+        public moveForward()
+        {
+            (int, int) North = (_r - 1, _c);
+            (int, int) South = (_r + 1, _c);
+            (int, int) East = (_r, _c - 1);
+            (int, int) West = (_r, _c + 1);
+
+            switch (Symbol)
+            {
+                case '|':
+                    // | is a vertical pipe connecting north and south.
+                    if (GtConfig.Instance.InRangeFromTo(North, South))
+                    {
+                        Coordinates.Add(new Coordinate(_symbol, _r, _c, North));
+                        Coordinates.Add(new Coordinate(_symbol, _r, _c, South));
+                    }
+                    break;
+                case '-':
+                    // - is a horizontal pipe connecting east and west.
+                    if (GtConfig.Instance.InRangeFromTo(South, West))
+                    {
+                        Coordinates.Add(new Coordinate(_symbol, _r, _c, South));
+                        Coordinates.Add(new Coordinate(_symbol, _r, _c, West));
+                    }
+                    break;
+                case 'L':
+                    // L is a 90 - degree bend connecting north and east.
+                    if (GtConfig.Instance.InRangeFromTo(North, East))
+                    {
+                        Coordinates.Add(new Coordinate(_symbol, _r, _c, North));
+                        Coordinates.Add(new Coordinate(_symbol, _r, _c, East));
+                    }
+                    break;
+                case 'J':
+                    // J is a 90 - degree bend connecting north and west.
+                    if (GtConfig.Instance.InRangeFromTo(North, West))
+                    {
+                        Coordinates.Add(new Coordinate(_symbol, _r, _c, North));
+                        Coordinates.Add(new Coordinate(_symbol, _r, _c, West));
+                    }
+                    break;
+                case '7':
+                    // 7 is a 90 - degree bend connecting south and west.
+                    if (GtConfig.Instance.InRangeFromTo(South, West))
+                    {
+                        Coordinates.Add(new Coordinate(_symbol, _r, _c, South));
+                        Coordinates.Add(new Coordinate(_symbol, _r, _c, West));
+                    }
+                    break;
+                case 'F':
+                    // F is a 90 - degree bend connecting south and east.
+                    if (GtConfig.Instance.InRangeFromTo(South, West))
+                    {
+                        Coordinates.Add(new Coordinate(_symbol, _r, _c, South));
+                        Coordinates.Add(new Coordinate(_symbol, _r, _c, West));
+                    }
+                    break;
+                case 'S':
+                    // S is the starting position of the animal; there is a pipe on this tile, but your sketch doesn't show what shape the pipe has.
+                    _startNode = new Coordinate(_symbol, _r, _c);
+                    Coordinates.Add(_startNode);
+                    break;
+                case '.':
+                    // . is ground; there is no pipe in this tile.
+                    // Console.WriteLine("No need to save thies coordinate!");
+                    break;
+                default:
+                    // Console.WriteLine("Shouldn't stopped by here!");
+                    break;
+            }
+        }
+    }
+}
---
+++ 2023-10/GtConfig.cs
@@ -0,0 +1,112 @@
+﻿namespace _2023_10
+{
+    enum status { deadend, ontrack, start, end };
+    public sealed class GtConfig
+    {
+
+        public List<Coordinate> Coordinates  { get; }
+
+        public char[,] Matrix { get; set; }
+        public int ROWS { get; private set; }
+        public int COLS { get; private set; }
+        public int STARTROW { get; set; }
+        public int STARTCOL { get; set; }
+        public int ENDROW { get; private set; }
+        public int ENDCOL { get; private set; }
+        public readonly char STARTLETTER = 'S';
+        public readonly char ENDLETTER = 'E';
+        public int SearchCnt { get; private set; } = 0;
+        private GtConfig()
+        {
+
+            // ParseFile("./data.txt");
+            Coordinates = new List<Coordinate>();
+        }
+        private static readonly Lazy<GtConfig> lazy = new Lazy<GtConfig>(() => new GtConfig());
+        public static GtConfig Instance
+        {
+            get
+            {
+                return lazy.Value;
+            }
+        }
+
+        public bool InRange(int row, int col)
+        {
+            return (row >= 0 && row < ROWS && col >= 0 && col < COLS);
+        }
+
+        internal bool InRangeFromTo((int, int) c1, (int, int) c2)
+        {
+            return InRange(c1.Item1, c1.Item2) && InRange(c2.Item1, c2.Item2);
+        }
+
+        public void IncreaseSearchCnt()
+        {
+            SearchCnt++;
+        }
+        void ParseFile(string filename)
+        {
+            string[] _filedata = File.ReadAllLines(filename);
+            ROWS = _filedata.Length;
+            COLS = 0;
+
+            foreach (string line in _filedata)
+            {
+                COLS = Math.Max(COLS, line.Length);
+            }
+
+            Matrix = new char[ROWS, COLS];
+
+            int _r = 0;
+            foreach (string line in _filedata)
+            {
+                char[] line_c = line.ToCharArray();
+                for (int _c = 0; _c < line_c.Length; _c++)
+                {
+                    Matrix[_r, _c] = line_c[_c];
+                    // Console.Write(Matrix[_r, _c]);
+                    if (line[_c] == 'S')
+                    {
+                        STARTROW = _r;
+                        STARTCOL = _c;
+                    }
+                    else if (line[(int)_c] == 'E')
+                    {
+                        ENDROW = _r;
+                        ENDCOL = _c;
+                    }
+                }
+                // Console.WriteLine();
+                _r++;
+            }
+
+        }
+
+        internal void AddNeighrbourIfValid(List<int> neighBoudList, char _currentLetter, int _r, int _c)
+        {
+            _currentLetter = _currentLetter == 'S' ? 'a' : _currentLetter;
+            _currentLetter = _currentLetter == 'E' ? 'z' : _currentLetter;
+
+            char _nextLetter = (char)((int)_currentLetter + 1);
+            if (InRange(_r, _c))
+            {
+                char _neighbour = GtConfig.Instance.Matrix[_r, _c];
+                if (_currentLetter == _neighbour || _nextLetter == _neighbour || 'E' == _neighbour)
+                {
+                    // int _node = (_r * 1000) + _c;
+                    int _node = GenerateKey(_r, _c);
+
+                    neighBoudList.Add(_node);
+                }
+            }
+        }
+
+        internal int GenerateKey(int r, int c)
+        {
+            return 10000000 + (r * 10000) + c;
+        }
+
+
+    }
+}
